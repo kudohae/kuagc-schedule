448ae08713fe54896d07c7afafc156e83d5cf43a
kudohae <dohe916@gmail.com>
2026-09-24 13:13:31 +0900

    Remove automatic formation reset control

--- js/index/main.js
+++ js/index/main.js
@@ -1,4 +1,4 @@
-import { initRouter } from '../router.js?v=20260924-applicant-member';
+import { initRouter } from '../router.js?v=20260924-manual-lock';
 import {
   getConfig, fetchTeams, fetchTeamCategories, fetchTeamKindDefaults, fetchBaseSlots, fetchExceptions, mergeSchedule,
   fetchRequests, createRequest, rejectRequest,
